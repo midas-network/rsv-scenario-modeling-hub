c6e5b85cfdfb93712791d63f5c002baa31e91685
LucieContamin <CONTAMIN@pitt.edu>
2023-10-03 11:00:49 -0400

    Update RSV data extract code
    - add RSV-Net Hospitalization data
    - Update NREVSS: add as_of date column; keep historical data; add national data

--- auxiliary-data/extract_rsv.py
+++ auxiliary-data/extract_rsv.py
@@ -1,4 +1,5 @@
-
+import os
+import io
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
@@ -27,55 +28,88 @@ def get_table_rows(table):
     return table_rows
 
 
-today = date.today()
-today_str = today.strftime("%Y-%m-%d")
+def get_nrevss_data(url_bases, url_ext, locations, location_name=None, folder_path="auxiliary-data/nrevss/"):
+    today = date.today()
+    today_str = today.strftime("%Y-%m-%d")
 
-url_bases = [
+    for u in url_bases:
+        headers = []
+        rows = []
+        filename = ""
+
+        for location in locations:
+            if location_name is None:
+                loc_name = location
+            else:
+                loc_name = location_name
+            try:
+                r = requests.get(u + location + url_ext)
+                if r.status_code == 200:
+
+                    # parse the html
+                    soup = BeautifulSoup(r.text, 'html.parser')
+
+                    # search for the <table> tags
+                    tables = soup.find_all("table")
+
+                    for t in tables:
+                        filename = (t.caption.text.strip().replace("for " + loc_name, "").
+                                    replace("Insufficient Antigen Data:  ", "").
+                                    replace("Insufficient Data:  ", "").lower().
+                                    replace(" ", "_").replace("(", "").replace(")", "") + ".csv")
+                        headers = get_table_headers(t)
+                        state_rows = get_table_rows(t)
+                        rows.extend(state_rows)
+
+                else:
+                    print("Error getting page; HTTP status " + str(r.status_code))
+
+            except Exception as e:
+                print("Error for state " + location + " and URL " + u + location + url_ext)
+                print(e)
+
+        # Add date information
+        df = pd.DataFrame(rows)
+        df = df.rename(columns=dict(zip(range(len(headers)), headers)))
+        df["repweekdate"] = pd.to_datetime(df["repweekdate"]).dt.strftime("%Y-%m-%d")
+        df["as_of"] = today_str
+
+        file_dir = folder_path + filename
+        if os.path.isfile(file_dir):
+            df0 = pd.read_csv(file_dir)
+            df_ts = set(list(df["repweekdate"].drop_duplicates())).intersection(
+                list(df0["repweekdate"].drop_duplicates()))
+            df0 = df0[~df0["repweekdate"].isin(df_ts)]
+            df_all = pd.concat([df0, df], ignore_index=True)
+        else:
+            df_all = df
+        # write the csv file
+        df_all.to_csv(file_dir, index=False)
+
+
+# RSV State Trends - The National Respiratory and Enteric Virus Surveillance System (NREVSS)
+state_bases = [
     "https://www.cdc.gov/surveillance/nrevss/images/rsvstate/RSV1PPCent3AVG_State",
     "https://www.cdc.gov/surveillance/nrevss/images/rsvstate/RSV4PPCent3AVG_State",
     "https://www.cdc.gov/surveillance/nrevss/images/rsvstate/RSV14NumCent5AVG_State"]
-url_ext = ".htm"
-
-locations = pd.read_csv("data-locations/locations.csv")
-states = list(locations["abbreviation"])
+hub_locations = pd.read_csv("data-locations/locations.csv")
+states = list(hub_locations["abbreviation"])
 to_remove = ["AS", "GU", "MP", "PR", "UM", "VI", "US"]
 for i in to_remove:
     states.remove(i)
+get_nrevss_data(state_bases, ".htm", states)
 
-for u in url_bases:
-    headers = []
-    rows = []
-    filename = ""
+# RSV National Trends - The National Respiratory and Enteric Virus Surveillance System (NREVSS)
+nat_bases = {"https://www.cdc.gov/surveillance/nrevss/images/trend_images/RSV124PP_",
+             "https://www.cdc.gov/surveillance/nrevss/images/trend_images/RSV14Num_"}
+get_nrevss_data(nat_bases, ".htm", ["Nat"], location_name="the ")
 
-    for state in states:
-        try:
-            r = requests.get(u + state + url_ext)
-            if r.status_code == 200:
-
-                # parse the html
-                soup = BeautifulSoup(r.text, 'html.parser')
-
-                # search for the <table> tags
-                tables = soup.find_all("table")
-
-                for t in tables:
-                    filename = (t.caption.text.strip().replace("for " + state, "").
-                                replace("Insufficient Antigen Data:  ", "").
-                                replace("Insufficient Data:  ", "").lower().
-                                replace(" ", "_").replace("(", "").replace(")", "") + ".csv")
-                    headers = get_table_headers(t)
-                    state_rows = get_table_rows(t)
-                    rows.extend(state_rows)
-
-            else:
-                print("Error getting page; HTTP status " + str(r.status_code))
-
-        except Exception as e:
-            print("Error for state " + state + " and URL " + u + state + url_ext)
-            print(e)
-
-    df = pd.DataFrame(rows)
-    df = df.rename(columns=dict(zip(range(len(headers)), headers)))
-    df["repweekdate"] = pd.to_datetime(df["repweekdate"]).dt.strftime("%Y-%m-%d")
-    # write the csv file
-    df.to_csv("auxiliary-data/nrevss/" + filename, index=False)
+# Weekly Rates of Laboratory-Confirmed RSV Hospitalizations - Respiratory Syncytial Virus Hospitalization
+# Surveillance Network (RSV-NET)
+res = requests.get("https://data.cdc.gov/api/views/29hc-w46k/rows.csv?accessType=DOWNLOAD")
+df_rsvnet = pd.read_csv(io.StringIO(res.text),
+                        dtype={'State': "string", 'Season': "string", 'MMWR Week': "string",
+                               'Week ending date': "string", 'Age Category': "string",
+                               'Sex': "string", 'Race': "string", 'Rate': "float",
+                               'Cumulative Rate': "float"})
+df_rsvnet.to_csv("auxiliary-data/rsv-net/weekly_rates_lab_confirmed_rsv_hosp.csv")
