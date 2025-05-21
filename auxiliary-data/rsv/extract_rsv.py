0977132feeb3a4b6e1ea4a8f0f88a3a315fe7a5f
LucieContamin <CONTAMIN@pitt.edu>
2025-05-21 16:29:05 -0400

    update code for data download

--- auxiliary-data/rsv/extract_rsv.py
+++ auxiliary-data/rsv/extract_rsv.py
@@ -1,40 +1,51 @@
-import os
 import io
 import requests
-from bs4 import BeautifulSoup
 from datetime import date
 import pandas as pd
 
-# Weekly Rates of Laboratory-Confirmed RSV Hospitalizations - Respiratory Syncytial Virus Hospitalization
-# Surveillance Network (RSV-NET)
-res = requests.get("https://data.cdc.gov/api/views/29hc-w46k/rows.csv?accessType=DOWNLOAD")
-df_rsvnet = pd.read_csv(io.StringIO(res.text),
-                        dtype={'State': "string", 'Season': "string", 'MMWR Week': "string",
-                               'Week ending date': "string", 'Age Category': "string",
-                               'Sex': "string", 'Race': "string", 'Rate': "float",
-                               'Cumulative Rate': "float"})
-df_rsvnet.to_parquet("auxiliary-data/rsv/rsv-net/" + str(date.today()) + 
-    "_weekly_rates_lab_confirmed_rsv_hosp.parquet")
+
+def request_get_link(link, output_dtype=None):
+    df = None
+    try:
+        result = requests.get(link, timeout=30)
+        if result.status_code == 200:
+            df = pd.read_csv(io.StringIO(result.text), dtype=output_dtype)
+        else:
+            print("Error getting page; HTTP status " + str(result.status_code))
+    except Exception as e:
+        print("Error for link: " + link)
+        print(e)
+    return df
+
+
+# Weekly Rates of Laboratory-Confirmed RSV Hospitalizations - Respiratory Syncytial Virus
+# Hospitalization Surveillance Network (RSV-NET)
+df_rsvnet = (
+    request_get_link("https://data.cdc.gov/api/views/29hc-w46k/rows.csv?accessType=DOWNLOAD",
+                     output_dtype={'State': "string", 'Season': "string", 'MMWR Week': "string",
+                                   'Week ending date': "string", 'Age Category': "string",
+                                   'Sex': "string", 'Race': "string", 'Rate': "float",
+                                   'Cumulative Rate': "float"}))
+df_rsvnet.to_parquet("auxiliary-data/rsv/rsv-net/" + str(date.today()) +
+                     "_weekly_rates_lab_confirmed_rsv_hosp.parquet")
 
 # Weekly Respiratory Virus Vaccination Data, Children 6 Months-17 Years and Adults 18 Years and 
 # Older, National Immunization Survey
-res = requests.get("https://data.cdc.gov/resource/5c6r-xi2t.csv")
-df_rsvvax = pd.read_csv(io.StringIO(res.text))
+df_rsvvax = request_get_link("https://data.cdc.gov/resource/5c6r-xi2t.csv")
 df_rsvvax.to_parquet("auxiliary-data/rsv/rsv-vax/weekly_respiratory_virus_vaccination_data.parquet")
 
 
 # Percent Positivity of Respiratory Syncytial Virus Nucleic Acid Amplification 
 # Tests by HHS Region, National Respiratory and Enteric Virus Surveillance System
-res = requests.get("https://data.cdc.gov/api/views/3cxc-4k8q/rows.csv?accessType=DOWNLOAD")
-df_pos = pd.read_csv(io.StringIO(res.text))
+df_pos = request_get_link("https://data.cdc.gov/api/views/3cxc-4k8q/rows.csv?accessType=DOWNLOAD")
 df_pos.to_parquet("auxiliary-data/rsv/nrevss/weekly_positivity.parquet")
 
 # 2023 Respiratory Virus Response - NSSP Emergency Department Visits - COVID-19,
 # Flu, RSV, Combined
-res = requests.get("https://data.cdc.gov//api/views/rdmq-nq56/rows.csv?accessType=DOWNLOAD")
-df_edstate = pd.read_csv(io.StringIO(res.text))
+df_edstate = (
+    request_get_link("https://data.cdc.gov//api/views/rdmq-nq56/rows.csv?accessType=DOWNLOAD"))
 df_edstate.to_parquet("auxiliary-data/rsv/nssp/ed_state.parquet")
 
-res = requests.get("https://data.cdc.gov//api/views/7xva-uux8/rows.csv?accessType=DOWNLOAD")
-df_eddemo = pd.read_csv(io.StringIO(res.text))
+df_eddemo = (
+    request_get_link("https://data.cdc.gov//api/views/7xva-uux8/rows.csv?accessType=DOWNLOAD"))
 df_eddemo.to_parquet("auxiliary-data/rsv/nssp/ed_demo.parquet")
